5f8c6dafb9d4674881519befe7bf954c2cf8a476
nhatlinh <nhatlinhdev201@gmail.com>
2023-11-08 08:44:12 +0700

    Nhat Linh : update structure folder app and update flow screen

--- App.js
+++ App.js
@@ -2,20 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
-import ScreenLogin from './src/screen/Account/ScreenLogin';
-import ScreenPasswordConfirm from './src/screen/Account/ScreenPasswordConfirm';
-import ScreenAccount from './src/screen/ScreenAccount';
-import ScreenDoctor from './src/screen/ScreenDoctor';
-import ScreenServicePackage from './src/screen/ScreenServicePackage';
-import ScreenPasswordLogin from './src/screen/Account/ScreenPasswordLogin';
-import ScreenHome from './src/screen/ScreenHome';
+import ScreenLogin from './src/screen/authentication/ScreenLogin';
+import ScreenPasswordConfirm from './src/screen/authentication/ScreenPasswordConfirm';
+import ScreenAccount from './src/screen/app/account/ScreenAccount';
+import ScreenDoctor from './src/screen/app/home/ScreenDoctor';
+import ScreenServicePackage from './src/screen/app/home/ScreenServicePackage';
+import ScreenPasswordLogin from './src/screen/authentication/ScreenPasswordLogin';
+import ScreenHome from './src/screen/app/home/ScreenHome';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() { 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='ScreenLogin'>
+      <Stack.Navigator initialRouteName='ScreenAccount'>
         <Stack.Screen
           name='ScreenLogin'
           component={ScreenLogin}
